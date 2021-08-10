84369fb785dfde11e3cb3b5bbde1ef58cdb47266
LiangHaoXiang <1013568397@qq.com>
2021-08-10 10:23:19 +0800

    Initial Commit

---
+++ OpenGL_Test22_NormalMapping/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpenGL_Test22_NormalMapping
+//
+//  Created by Haoxiang Liang on 2021/8/10.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
